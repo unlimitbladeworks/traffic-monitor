befa3695c8fad8c9903c15650b74cf49abf26e1e
unlimitbladeworks <13121347080@sina.cn>
2018-06-18 20:56:39 +0800

    add amap_api

---
+++ data_source/amap_api.py
@@ -0,0 +1,36 @@
+#coding = utf-8
+
+"""
+@author: sy
+
+@file: amap_api.py
+
+@time: 2018/6/3 18:59
+
+@desc: 高德地图英文名称
+
+    http://lbs.amap.com/api/webservice/gettingstarted 申请key值
+
+
+"""
+
+import requests
+
+
+class ReadMapInfo:
+
+    #请求url方法
+    def request_url(self):
+        pass
+
+
+    #获取地理编码方法
+    def read_geo(self):
+        pass
+
+
+    #获取指定路线交通趋势
+    def read_road(self):
+        pass
+
+
