435291fcf5486dafcce79e30b6698f2fad430337
unlimitbladeworks <13121347080@sina.cn>
2018-08-19 21:39:41 +0800

    add request_url function

--- data_source/amap_api.py
+++ data_source/amap_api.py
@@ -18,13 +18,24 @@
 """
 
 import requests
+from requests import RequestException
 
 
 class ReadMapInfo:
 
     #请求url方法
-    def request_url(self):
-        pass
+    def request_url(self,url):
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
+        }
+        try:
+            r = requests.get(url=url,headers=headers)
+            if r.status_code == 200:
+                return r.text
+            return None
+        except RequestException:
+            print('请求url返回错误异常')
+            return None
 
 
     #获取地理编码方法
@@ -35,5 +46,6 @@ class ReadMapInfo:
     #获取指定路线交通趋势
     def read_road(self):
         pass
+
 
 
