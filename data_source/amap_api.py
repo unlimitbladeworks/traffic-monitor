18faed51bd8ac8ac95628dc1772fbea5525b7a48
unlimitbladeworks <13121347080@sina.cn>
2018-08-20 22:53:10 +0800

    add parse_json funciton and TODO read_geo

--- data_source/amap_api.py
+++ data_source/amap_api.py
@@ -19,6 +19,7 @@
 
 import requests
 from requests import RequestException
+import json
 
 
 # 获取地图信息类
@@ -43,13 +44,24 @@ class ReadMapInfo:
         """ 请求地理编码的url地址 """
         url = f'https://restapi.amap.com/v3/geocode/geo?city={city}&address={address}&&key={key}'
         r = self.request_url(url)
-        print(r)
+        result_json = self.parse_json(r)
+        # 如果请求成功则进行处理,否则返回空
+        if result_json['status'] == 1:
+            # TODO 待添加
+            pass
+        else:
+            return None
 
     # 获取指定路线交通趋势
     def read_road(self):
         pass
 
+    # 解析json函数
+    def parse_json(self, content_json):
+        result_json = json.loads(content_json)
+        return result_json
+
 
 if __name__ == '__main__':
     readMapInfo = ReadMapInfo()
-    readMapInfo.read_geo('北京', '方恒国际中心A座', '439b13eba868071ba3d294a07c2bc573')
+    readMapInfo.read_geo('北京', '方恒国际中心A座', '')
