98138625024e6d83f0b5a8c71f9cd39c4eab9db0
unlimitbladeworks <13121347080@sina.cn>
2018-08-20 22:13:08 +0800

    add read_geo function

--- data_source/amap_api.py
+++ data_source/amap_api.py
@@ -1,4 +1,4 @@
-#coding = utf-8
+# coding = utf-8
 
 """
 @author: sy
@@ -21,15 +21,17 @@ import requests
 from requests import RequestException
 
 
+# 获取地图信息类
 class ReadMapInfo:
 
-    #请求url方法
-    def request_url(self,url):
+    # 请求url方法
+    @staticmethod
+    def request_url(url):
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
         }
         try:
-            r = requests.get(url=url,headers=headers)
+            r = requests.get(url=url, headers=headers)
             if r.status_code == 200:
                 return r.text
             return None
@@ -37,15 +39,19 @@ class ReadMapInfo:
             print('请求url返回错误异常')
             return None
 
+    # 获取地理编码方法,城市、具体路段、开发者key
+    @staticmethod
+    def read_geo(city, address, key):
+        """ 请求地理编码的url地址 """
+        url = f'https://restapi.amap.com/v3/geocode/geo?city={city}&address={address}&&key={key}'
+        r = ReadMapInfo.request_url(url)
+        print(r)
 
-    #获取地理编码方法
-    def read_geo(self):
+    # 获取指定路线交通趋势
+    @staticmethod
+    def read_road():
         pass
 
 
-    #获取指定路线交通趋势
-    def read_road(self):
-        pass
-
-
-
+if __name__ == '__main__':
+    ReadMapInfo.read_geo('北京', '官庄路', '')
