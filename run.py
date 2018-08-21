81687e0189f2bb3254557d10b071054a4d1aafe3
unlimitbladeworks <13121347080@sina.cn>
2018-08-21 23:05:34 +0800

    add main function

---
+++ run.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+@author: sy
+
+@file: run.py
+
+@time: 2018年08月21日23:04:01
+
+@desc: traffic-monitor main function
+
+"""
+
+
+# 入口函数
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
