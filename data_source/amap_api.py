02e670d682d5b85422ecf37c37a0741bbd093351
unlimitbladeworks <13121347080@sina.cn>
2018-08-24 20:49:42 +0800

    add read_road 's comment and modify kwargs

--- data_source/amap_api.py
+++ data_source/amap_api.py
@@ -24,7 +24,6 @@ import json
 
 # 获取地图信息类
 class ReadMapInfo:
-
     # 请求url方法
     def request_url(self, url):
         headers = {
@@ -56,7 +55,17 @@ class ReadMapInfo:
             return None
 
     # 获取指定路线交通趋势,道路模式默认矩形
-    def read_road(self, city, address, key, select_road_mode='rectangle'):
+    def read_road(self, city, address, key, **kwargs):
+        """ 必传参数: 城市,地址,用户key;
+            可选参数: select_road_mode(交通趋势)
+                        1.rectangle (矩形区域交通态势)
+                        2.circle    (圆形区域交通态势)
+                        3.road      (指定路线交通态势) ***此方式貌似有Bug(待解决)***
+        """
+        for key in kwargs:
+            setattr(key, kwargs[key])
+
+        select_road_mode = kwargs['select_road_mode']
         adcode, location = self.read_geo(city, address, key)
         """ 根据客户端传入的标识选择不同的道路模式:rectangle(矩形),circle(圆形),road(指定路线)"""
         if select_road_mode == 'rectangle':
@@ -67,7 +76,7 @@ class ReadMapInfo:
             url = f'https://restapi.amap.com/v3/traffic/status/circle?location={location}&key={key}'
         if select_road_mode == 'road':
             # 请求指定路线交通趋势的url地址
-            url = f'https://restapi.amap.com/v3/traffic/status/road?name={address}&adcode={adcode}&key={key}'
+            url = f'https://restapi.amap.com/v3/traffic/status/road?name={address}&adcode={adcode}&level={level}&key={key}'
         r = self.request_url(url)
         print(r)
 
@@ -79,4 +88,5 @@ class ReadMapInfo:
 
 if __name__ == '__main__':
     readMapInfo = ReadMapInfo()
-    readMapInfo.read_road('北京', '福润四季', '', select_road_mode='road')
+    readMapInfo.read_road('北京', '管庄路', '439b13eba868071ba3d294a07c2bc573', select_road_mode='road', level=6)
+    readMapInfo.read_road('北京', '管庄路', '439b13eba868071ba3d294a07c2bc573')
