c284725c18b80e5a9de52677856044555e14b710
unlimitbladeworks <13121347080@sina.cn>
2018-08-26 11:28:17 +0800

    add utils module and read user the key

---
+++ utils/__init__.py
@@ -0,0 +1,12 @@
+#coding = utf-8
+
+"""
+@author: sy
+
+@file: __init__.py.py
+
+@time: 2018/8/26 11:01
+
+@desc:
+
+"""
---
+++ utils/util.py
@@ -0,0 +1,23 @@
+# coding = utf-8
+
+"""
+@author: sy
+
+@file: util.py
+
+@time: 2018/8/26 11:01
+
+@desc: 工具类
+
+"""
+import os
+
+
+# 持久化key,便于读取
+def read_key():
+    # 获取key文件的绝对路径
+    key_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'user_key')
+    with open(key_path, 'r', encoding='utf-8') as f:
+        key = f.read()
+        f.close()
+    return key
