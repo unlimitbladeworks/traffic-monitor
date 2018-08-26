d505e889012ef8d7a5d9e97022a1c5031bdadb89
unlimitbladeworks <13121347080@sina.cn>
2018-08-26 11:22:59 +0800

    more deal the user_key

--- data_source/amap_api.py
+++ data_source/amap_api.py
@@ -23,9 +23,15 @@ import requests
 from requests import RequestException
 import json
 
-
 # 获取地图信息类
+from utils import util
+
+
 class ReadMapInfo:
+    # 初始化key
+    def __init__(self):
+        self.key = util.read_key()
+
     # 请求url方法get方法
     def request_url_get(self, url):
         headers = {
@@ -54,10 +60,10 @@ class ReadMapInfo:
             print('请求url返回错误异常')
             return None
 
-    # 获取地理编码方法,args:城市、具体路段、开发者key
-    def read_geo(self, city, address, key):
+    # 获取地理编码方法(adcode, location),args:城市、具体路段
+    def read_geo(self, city, address):
         """ 请求地理编码的url地址 """
-        url = f'https://restapi.amap.com/v3/geocode/geo?city={city}&address={address}&&key={key}'
+        url = f'https://restapi.amap.com/v3/geocode/geo?city={city}&address={address}&&key={self.key}'
         r = self.request_url_get(url)
         result_json = self.parse_json(r)
         # 如果请求成功则进行处理,否则返回空,获取城市标识
@@ -71,15 +77,15 @@ class ReadMapInfo:
             return None
 
     # 获取指定路线交通趋势,道路模式默认矩形
-    def read_road(self, city, address, key, **kwargs):
+    def read_road(self, city, address, **kwargs):
         """ 必传参数: 城市,地址,用户key;
             可选参数: select_road_mode(交通趋势)
                         1.rectangle (矩形区域交通态势)
                         2.circle    (圆形区域交通态势)
                         3.road      (指定路线交通态势) ***此方式貌似有Bug(待解决)***
         """
-        for key in kwargs:
-            setattr(key, kwargs[key])
+        for arg_name in kwargs:
+            setattr(arg_name, kwargs[arg_name])
 
         """ 若有参数传入,则选择客户端的参数,否则默认矩形区域交通态势 """
         if kwargs['select_road_mode']:
@@ -97,24 +103,34 @@ class ReadMapInfo:
         else:
             select_road_mode = 'rectangle'
 
-        adcode, location = self.read_geo(city, address, key)
+        adcode, location = self.read_geo(city, address)
         """ 根据客户端传入的标识选择不同的道路模式:rectangle(矩形),circle(圆形),road(指定路线)"""
         if select_road_mode == 'rectangle':
             # 矩形url查询经度和纬度使用","分隔坐标之间使用";"分隔.例如：x1,y1;x2,y2
-            url = f'https://restapi.amap.com/v3/traffic/status/rectangle?rectangle={rectangle}&key={key}'
+            url = f'https://restapi.amap.com/v3/traffic/status/rectangle?rectangle={rectangle}&key={self.key}'
         if select_road_mode == 'circle':
             # 半径参数默认1000m
-            url = f'https://restapi.amap.com/v3/traffic/status/circle?location={location}&key={key}'
+            url = f'https://restapi.amap.com/v3/traffic/status/circle?location={location}&key={self.key}'
         if select_road_mode == 'road':
             # 请求指定路线交通趋势的url地址
-            url = f'https://restapi.amap.com/v3/traffic/status/road?name={address}&adcode={adcode}&key={key}'
+            url = f'https://restapi.amap.com/v3/traffic/status/road?name={address}&adcode={adcode}&key={self.key}'
         r = self.request_url_get(url)
         print(r)
 
     # 通过拾取器获取详细的坐标,通过关键词进行搜索,返回具体详细坐标
-    def read_picker(self, keyword='天安门'):
+    def read_picker(self, city, keyword='天安门'):
+        # AMap 地图拾取后台请求地址:
         url = 'https://restapi.amap.com/v3/place/text'
-        self.request_url_post(url)
+
+        data = {
+            's': 'rsv3',
+            'language': 'zh_cn',
+            'key': '8325164e247e15eea68b59e89200988b',
+            'appname': 'https://lbs.amap.com/console/show/picker',
+            'csid': '99F8A6BF-BF82-4E40-8036-9F0692F0CEB1',
+            'keyword': keyword
+        }
+        result_json = self.request_url_post(url, data)
         pass
 
     # 解析json函数
